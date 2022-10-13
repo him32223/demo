f83efb88c225161e29346b93330ab2be33903655
him32223 <65442365+him32223@users.noreply.github.com>
2022-10-13 10:11:58 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/LolApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolApplication.class, args);
+	}
+
+}
